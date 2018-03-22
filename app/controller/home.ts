7a1ceb87242b7eeb5d558eb7cb99178af949bb89
wanghx <whx89768@alibaba-inc.com>
2018-03-22 21:48:08 +0800

    feat: update controller

--- app/controller/home.ts
+++ app/controller/home.ts
@@ -10,7 +10,22 @@ export default class HomeController extends Controller {
     if (ctx.isAjax()) {
       ctx.body = { time };
     } else {
-      ctx.body = time;
+      ctx.body = `
+      <div id="time" style="text-align: center;font-size: 24px;padding: 30px 0;">${time}</div>
+      <script>
+        setInterval(function() {
+          var xhr = new XMLHttpRequest();
+          xhr.open('GET', '/');
+          xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+          xhr.send();
+          xhr.onreadystatechange = function() {
+            if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+              document.getElementById('time').innerText = JSON.parse(xhr.responseText).time;
+            }
+          }
+        }, 1000);
+      </script>
+      `;
     }
   }
 
--- test/app/extend/context.test.ts
+++ test/app/extend/context.test.ts
@@ -17,6 +17,6 @@ describe('test/app/extend/context.test.js', () => {
 
   it('context should has isProd and isAjax', async () => {
     assert(typeof ctx.isProd === 'boolean');
-    assert(typeof ctx.isAjax === 'boolean');
+    assert(typeof ctx.isAjax() === 'boolean');
   });
 });
